57dc7d27eeff28e152e88e7d4f7152d0296cc910
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-10-01 21:51:04 +0200

    Adding search route handler to api.py

--- api.py
+++ api.py
@@ -7,3 +7,9 @@ templates = Jinja2Templates(directory='templates')
 @app.get("/", response_class=HTMLResponse)
 def homeRoute(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post('/search')
+def search(ticker_lookup: str = Form(...)):
+    print(ticker_lookup)
+    return f"<b>{ticker_lookup}</b>"
